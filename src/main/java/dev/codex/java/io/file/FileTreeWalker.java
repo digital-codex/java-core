2b06f11b90cfc5a7ddfb3f26d690d86fd98ab47e
treyvon <Eugenewhitaker2022@gmail.com>
2024-03-30 20:42:22 -0400

    feat(core): added logic to track visited directories

--- src/main/java/dev/codex/java/io/file/FileTreeWalker.java
+++ src/main/java/dev/codex/java/io/file/FileTreeWalker.java
@@ -4,7 +4,6 @@ import dev.codex.java.lang.Result;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
